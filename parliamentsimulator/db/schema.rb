9d1bd5247953e975333a3371d16b92d6a0878fbf
--replace-all <niko.j.heikkila@student.jyu.fi>
2015-03-10 11:11:06 +0200

    Reset to previous database setup

--- parliamentsimulator/db/schema.rb
+++ parliamentsimulator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226131337) do
+ActiveRecord::Schema.define(version: 20150310090806) do
 
   create_table "parties", force: :cascade do |t|
     t.string   "name"
@@ -20,11 +20,15 @@ ActiveRecord::Schema.define(version: 20150226131337) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "parties_values", id: false, force: :cascade do |t|
+    t.integer "value_id", null: false
+    t.integer "party_id", null: false
+  end
+
   create_table "values", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "position"
   end
 
 end
